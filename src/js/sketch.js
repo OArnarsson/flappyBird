4b41e39ca1511f653f67d776bcbc9d31d2a2bd69
OArnarsson <orriarnars@gmail.com>
2017-03-17 19:01:34 +0000

    MVP almost ready, some minor optimizations ready
    TODO: Check for hits, stop when dead. Track score, insert sprites, make
    good, make cool.

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,12 +1,40 @@
+let pipes = [];
+const MAXSPEED = 13;
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+  distance = 0;
 }
 
+// Happens every frame
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0);
+
+  // Update bird location and velocity
+  this.bird.update();
+  this.bird.show();
+  this.distance += 0.1;
+
+  // Loop through all pipes to update location
+  for (let pipe of pipes) {
+    pipe.show();
+    pipe.update();
+
+    // Delete pipe if we cannot see it
+    if (!pipe.onScreen()) {
+      pipes.splice(pipes.indexOf(pipe), 1);
+    }
   }
-  ellipse(mouseX, mouseY, 10, 10);
+
+  // Every 121 frames we create a new pipe
+  if (frameCount % 121 == 0) {
+    pipes.push(new Pipe());
+  }
+}
+
+// Flap the bird when mouse is pressed
+function mousePressed() {
+  this.bird.flap();
 }
