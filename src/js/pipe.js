e00283f7dac5bfbeff3f6970dfc3c1b4d37325a1
OArnarsson <orriarnars@gmail.com>
2017-03-17 18:59:37 +0000

    Pipes class - WIP
    TODO: Check for hits when the bird crashes to pipe.

---
+++ src/js/pipe.js
@@ -0,0 +1,25 @@
+function Pipe() {
+  this.upper = random(height/2);
+  this.lower = random(height/2);
+  this.x = width;
+  this.width = width/7.55;
+  this.velocity = 2.5;
+
+  // Display
+  this.show = () => {
+    fill(222);
+    rect(this.x, 0, this.width, this.upper);
+    rect(this.x, height-this.lower, this.width, this.lower);
+  }
+
+  // Happens every frame
+  this.update= () => {
+    // Update pipe location
+    this.x -= this.velocity;
+  }
+
+  // If we can see it, return true
+  this.onScreen= () => {
+    return this.x > -this.width;
+  }
+}
