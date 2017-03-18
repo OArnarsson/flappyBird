9bb5ae1d17de422c2c9c886027b2905ceeba1a32
OArnarsson <orriarnars@gmail.com>
2017-03-18 11:52:34 +0000

    Switched to raw node.js server for heroku

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
-const app = express();
 const fs = require('fs');
 
-//Parses all data coming through.
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: false }));
-
-//Middleware
-app.use((req, res, next) => {
-    let now = new Date().toString().slice(0, 24);
-    let log = `${now}: ${req.method} ${req.url}`;
-    console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err)
-            console.log('Cannot append to server.log file.');
-    });
-    next();
-});
-
-//Points to dist folder
-app.use(express.static('./src'));
-
-// All other routes return the index file
-app.get('*', (req, res) => {
-    return res.sendFile('/src/index.html', { root: './' });
-});
-
-// Server setup
 const port = process.env.PORT || 3000;
-app.set('port', port);
-const server = http.createServer(app);
+const server = http.createServer(handleRequest);
 server.listen(port, () => console.log(`Server running on port:${port}`));
+
+function handleRequest(req, res) {
+  let pathname = req.url;
+  
+  if (pathname == '/') {
+    pathname = '/src/index.html';
+  }
+  
+  let ext = path.extname(pathname);
+
+  let typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+
+  let contentType = typeExt[ext] || 'text/plain';
+
+  fs.readFile(__dirname + pathname,
+    (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        console.log(err);
+        return res.end('Error loading ' + pathname);
+      }
+
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
