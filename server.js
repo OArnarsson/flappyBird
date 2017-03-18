5cc7655002c9cf2fa21139e1ae824e6bdeb14a06
OArnarsson <orriarnars@gmail.com>
2017-03-18 11:34:16 +0000

    heroku port fix

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
 });
 
 // Server setup
-const port = '3000';
+const port = process.env.PORT || 3000;
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Server running on port:${port}`));
