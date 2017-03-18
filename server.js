6db3c2717003b126426dcbe8ce8fd03ea9c9b3fd
OArnarsson <orriarnars@gmail.com>
2017-03-18 11:23:33 +0000

    node server ready for heroku?

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+const fs = require('fs');
+
+//Parses all data coming through.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: false }));
+
+//Middleware
+app.use((req, res, next) => {
+    let now = new Date().toString().slice(0, 24);
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log('Cannot append to server.log file.');
+    });
+    next();
+});
+
+//Points to dist folder
+app.use(express.static('./src'));
+
+// All other routes return the index file
+app.get('*', (req, res) => {
+    return res.sendFile('/src/index.html', { root: './' });
+});
+
+// Server setup
+const port = '3000';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Server running on port:${port}`));
