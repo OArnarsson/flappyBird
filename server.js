74aa29db5828151c8f9dfe66cb13bfb8220084a5
OArnarsson <orriarnars@gmail.com>
2017-03-21 20:44:38 +0000

    server update

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
+const express = require('express');
 const path = require('path');
 const http = require('http');
-const fs = require('fs');
+const app = express();
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(handleRequest);
+//Points to dist folder
+app.use(express.static('./skil4/app'));
+
+// All other routes return the index file
+app.get('*', (req, res) => {
+        return res.sendFile('index.html', { root: './' });
+});
+
+// Server setup
+const port = process.env.PORT || 4200;
+app.set('port', port);
+const server = http.createServer(app);
 server.listen(port, () => console.log(`Server running on port:${port}`));
-
-function handleRequest(req, res) {
-  let pathname = req.url;
-  
-  if (pathname == '/') {
-    pathname = '/skil4/app/index.html';
-  }
-  
-  let ext = path.extname(pathname);
-
-  let typeExt = {
-    '.html': 'text/html',
-    '.js':   'text/javascript',
-    '.css':  'text/css',
-    '.tff':  'font/truetype'
-  };
-
-  let contentType = typeExt[ext] || 'text/plain';
-
-  fs.readFile(__dirname + pathname,
-    (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        console.log(err);
-        return res.end('Error loading ' + pathname);
-      }
-
-      res.writeHead(200,{ 'Content-Type': contentType });
-      res.end(data);
-    }
-  );
-}
